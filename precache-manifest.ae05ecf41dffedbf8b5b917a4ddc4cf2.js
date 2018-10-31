6017d1a5f5928bb74257149f52a1bd30766f2ce5
Kayer Arzadon-Smith <kayerasmith@icloud.com>
2018-10-31 14:18:14 -0700

    Updates

---
+++ precache-manifest.ae05ecf41dffedbf8b5b917a4ddc4cf2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "95e95a9b2b3c64bda1a6bc01a203602c",
+    "url": "/amazingkids/static/media/infantPic414.95e95a9b.jpg"
+  },
+  {
+    "revision": "8b8ff9aa2cd5a01b6131",
+    "url": "/amazingkids/static/css/main.86d019bd.chunk.css"
+  },
+  {
+    "revision": "9b715b5f8693bba67b76",
+    "url": "/amazingkids/static/js/1.9b715b5f.chunk.js"
+  },
+  {
+    "revision": "0ff8a34ec097679f62b7",
+    "url": "/amazingkids/static/js/runtime~main.0ff8a34e.js"
+  },
+  {
+    "revision": "985eafd981912dfd00c2f75f20d75b1d",
+    "url": "/amazingkids/static/media/NancyJohnson.985eafd9.jpg"
+  },
+  {
+    "revision": "0d0781ce8e29b7d10fdcb491523ecd72",
+    "url": "/amazingkids/static/media/veneta .0d0781ce.jpg"
+  },
+  {
+    "revision": "efe5c627a8c5fa3ec83f068ccbd500e9",
+    "url": "/amazingkids/static/media/Rex.efe5c627.jpg"
+  },
+  {
+    "revision": "3b4e46632d1135dfc226c310f77b5eb8",
+    "url": "/amazingkids/static/media/Kindergarden.3b4e4663.jpg"
+  },
+  {
+    "revision": "408a94a1d6e35ea4c2b4fc8777363630",
+    "url": "/amazingkids/static/media/prek.408a94a1.jpg"
+  },
+  {
+    "revision": "283c5c88a751cd2b7e7138c19aa1bbc4",
+    "url": "/amazingkids/static/media/infant3.283c5c88.jpg"
+  },
+  {
+    "revision": "96ec2c3600e0f9b013c48620d42f6eb0",
+    "url": "/amazingkids/static/media/amazingKids414.96ec2c36.jpg"
+  },
+  {
+    "revision": "e670af11de8cdeed7aced783a2882bcd",
+    "url": "/amazingkids/static/media/amazingKids500.e670af11.jpg"
+  },
+  {
+    "revision": "ef350b5ae8010b7aa89142f30d9e202b",
+    "url": "/amazingkids/static/media/amazingKidsMain.ef350b5a.jpg"
+  },
+  {
+    "revision": "e078bff67a50dc284f217e5dacfe3feb",
+    "url": "/amazingkids/static/media/infantPic375.e078bff6.jpg"
+  },
+  {
+    "revision": "8b8ff9aa2cd5a01b6131",
+    "url": "/amazingkids/static/js/main.8b8ff9aa.chunk.js"
+  },
+  {
+    "revision": "997123fdb937209e0325387efd1829ea",
+    "url": "/amazingkids/static/media/infantPic500.997123fd.jpg"
+  },
+  {
+    "revision": "e93deaba016fdd3073239f79f1dfdb38",
+    "url": "/amazingkids/static/media/infant500.e93deaba.jpg"
+  },
+  {
+    "revision": "b78e235b71ee8421ea3eed9a25a0236a",
+    "url": "/amazingkids/static/media/toddlerPic375.b78e235b.jpg"
+  },
+  {
+    "revision": "8328fac3c30d7cf7be2bc28ba980ed7b",
+    "url": "/amazingkids/static/media/toddler414.8328fac3.jpg"
+  },
+  {
+    "revision": "b7dd4c4ea89573ba60b97c9ba32ae0c8",
+    "url": "/amazingkids/static/media/toddlerPic500.b7dd4c4e.jpg"
+  },
+  {
+    "revision": "d7175a4b3d28058aa6aa62f46ff8e05b",
+    "url": "/amazingkids/static/media/toddler500.d7175a4b.jpg"
+  },
+  {
+    "revision": "f12640d9f42911b3bc19a59656345c44",
+    "url": "/amazingkids/static/media/preKPic375.f12640d9.jpg"
+  },
+  {
+    "revision": "e0d7c85ef10a4d896dce7e460776f2cf",
+    "url": "/amazingkids/static/media/preK414.e0d7c85e.jpg"
+  },
+  {
+    "revision": "acd26e0a3f3e7a934a0b83dd495889f3",
+    "url": "/amazingkids/static/media/preKPic500.acd26e0a.jpg"
+  },
+  {
+    "revision": "4e746ccc40652a77d1b9a466c49f8fcb",
+    "url": "/amazingkids/static/media/preK500.4e746ccc.jpg"
+  },
+  {
+    "revision": "2db28c5b7f90423645bf907757a84ca4",
+    "url": "/amazingkids/static/media/kinderPrep375.2db28c5b.jpg"
+  },
+  {
+    "revision": "282640dda8c34f4ca68784f859c5245a",
+    "url": "/amazingkids/static/media/kinderPrep500.282640dd.jpg"
+  },
+  {
+    "revision": "bb60b81d520b21bb11875374331c20a6",
+    "url": "/amazingkids/index.html"
+  }
+];
